a599e3021d0a6153243be02b81b7d279a5c0b781
Máté Vágó <ssmoke55555@gmail.com>
2015-06-01 21:32:43 +0200

    Update documentation.

--- doc/html/search/functions_5.js
+++ doc/html/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['isipv6',['isIPv6',['../classtnnf_1_1_address.html#ab09c57b4f764a8bcdf185486a1973caf',1,'tnnf::Address']]],
-  ['ispacketsstored',['isPacketsStored',['../classtnnf_1_1_packet_buffer.html#af43633c6f4756697a7fafe3bd85d5663',1,'tnnf::PacketBuffer']]]
+  ['ispacketstored',['isPacketStored',['../classtnnf_1_1_packet_buffer.html#a79ac7560d2e7dda075eacbfb0d7ea572',1,'tnnf::PacketBuffer']]]
 ];
